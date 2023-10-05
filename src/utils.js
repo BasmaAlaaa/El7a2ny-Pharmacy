d903d709bae14c0310068d99519839f3d35f03f4
Mireille Bishay <110732750+mireillebishay@users.noreply.github.com>
2023-10-05 18:01:34 +0300

    updating utils to look for admin usernames in DB

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,12 @@
 const patientModel = require('./Models/patient.js');
 const PharmacistRegistrationRequest = require('./Models/pharmacistRegistrationRequest');
+const administratorModel = require('./Models/administrator.js');
 
 async function isUsernameUnique(username) {
   const patientExists = await patientModel.findOne({ Username: username });
   const pharmacistExists = await PharmacistRegistrationRequest.findOne({ Username: username });
-  return !patientExists && !pharmacistExists;
+  const adminExists = await administratorModel.findOne({ Username: username });
+  return !patientExists && !pharmacistExists && !adminExists;
 }
 
 async function isEmailUnique(email) {
