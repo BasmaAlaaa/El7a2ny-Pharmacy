671c8c536d9c57e07e4c9b95b1af1d01989546ea
Basma Alaa Mahmoud <121583270+basmaAlaaElSayed@users.noreply.github.com>
2023-11-11 00:48:11 +0200

    Login frontend fadel heta hakhlsha el sobh

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,47 +1,67 @@
-import { useNavigate } from 'react-router-dom';
-import Form from '../components/Form.jsx';
-import { useDispatch } from 'react-redux';
-import { loggedIn } from '../features/login.js';
-import Validation from '../validate/validate';
-import Input from '../components/Input.jsx';
-import MainBtn from '../components/Button.jsx';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-
+import { useNavigate } from "react-router-dom";
+import Form from "../components/Form.jsx";
+import { useDispatch } from "react-redux";
+import { loggedIn } from "../features/login.js";
+import Validation from "../validate/validate";
+import Input from "../components/Input.jsx";
+import MainBtn from "../components/Button.jsx";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 function Login() {
-  let { errors, handleSubmit, register } = Validation('login')
+  let { errors, handleSubmit, register } = Validation("login");
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
 
-  
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:4000/login", {
+        Username: username,
+        password: password,
+      });
+      localStorage.setItem("token", response.data.token);
+      if (response.data.userPatient) {
+        navigate(`/patientView/${username}`);
+      } else if (response.data.userpharmacist) {
+        navigate(`/pharmacistView/${username}`);
+      } else if (response.data.userAdmin) {
+        navigate(`/administratorView/${username}`);
+      } else {
+        console.error("User role not recognized");
+        alert("User role not recognized");
+      }
+    } catch (error) {
+      console.error(error.response ? error.response.data : error.message);
+      alert(error.response ? error.response.data.error : error.message);
+    }
+  };
 
   return (
     <div>
       {/* <Form title="login" inputArr={inputArr} type="login" btnArr={btnArr} /> */}
-      <form
-      className="d-flex justify-content-center"
-    >
-      <div className="form-width">
-        <p className="text-capitalize fs-4">Login</p>
- 
+      <form className="d-flex justify-content-center">
+        <div className="form-width">
+          <p className="text-capitalize fs-4">Login</p>
+
           <Input
-            title='email'
-            placeholder='enter your email'
-            type='email'
-            onChange={(e) => setEmail(e.target.value)}
+            title="username"
+            placeholder="enter your username"
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
           />
           <Input
-            title='password'
-            placeholder='enter your password'
-            type='password'
+            title="password"
+            placeholder="enter your password"
+            type="password"
             onChange={(e) => setPassword(e.target.value)}
           />
-      
 
-        {/* {type == 'register' && (
+          {/* {type == 'register' && (
           <p className="text-center mt-3 small-txt">
             By Creating an account you agree to our
             <a className="green-txt"> Terms of use </a>
@@ -52,15 +72,15 @@ function Login() {
 
           <div className="mt-3">
             <MainBtn
-              txt='login'
-              style='green-btn'
+              txt="login"
+              style="green-btn"
               // action={handleSubmit(c)}
-              
+              action={handleLogin}
             />
           </div>
 
-        <p className="text-center mt-3 small-txt">
-          {/* {type == 'register' ? (
+          <p className="text-center mt-3 small-txt">
+            {/* {type == 'register' ? (
             <>
               Have an account?
               <Link to="/" className="green-txt">
@@ -72,19 +92,17 @@ function Login() {
             <>
               Forgot Password?
               <Link to="/forgotpassword" className="green-txt">
-                {' '}
+                {" "}
                 Reset Password
               </Link>
             </>
-          {/* ) : (
+            {/* ) : (
             ''
           )} */}
-        </p>
-      </div>
-    </form>
+          </p>
+        </div>
+      </form>
     </div>
-    
   );
 }
 export default Login;
-
