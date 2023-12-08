c906dce5658269c036c04fc30679f8d6a05c4162
Mireille Bishay <110732750+mireillebishay@users.noreply.github.com>
2023-12-08 21:47:56 +0200

    update editMedicine.jsx

--- frontend/src/pages/editMedicine.jsx
+++ frontend/src/pages/editMedicine.jsx
@@ -1,63 +1,60 @@
 import { useNavigate, useParams } from 'react-router-dom';
-import Form from '../components/Form.jsx';
 import { useDispatch } from 'react-redux';
-import { loggedIn } from '../features/login.js';
-import Validation from '../validate/validate';
-import NavBarPharmacist from '../components/NavBarPharmacist.jsx'
-import { useState } from 'react';
 import axios from 'axios';
+import NavBarPharmacist from '../components/NavBarPharmacist.jsx';
+import { useState } from 'react';
 
 function EditMedicine() {
-  const { name, username } = useParams();
+  const { username, name } = useParams();
   const [activeIngredients, setActiveIngredients] = useState('');
   const [price, setPrice] = useState(0);
   const [picture, setPicture] = useState('');
 
-  // let { errors, handleSubmit, register } = Validation('username')
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  // let c = (data) => {
-  //   console.log(data);
-  // }
-  // let inputArr = [
-  //   { title: 'activeIngredients', placeholder: 'enter active ingredients', type: 'activeIngredients', showErr: errors.activeIngredients?.message, register: register("activeIngredients") },
-  //   { title: 'price', placeholder: 'enter price', type: 'price', showErr: errors.price?.message, register: register("price") },
-  // ];
-  // let btnArr = [
-  //   {
-  //     title: 'Edit Medicine',
-  //     style: 'green-btn',
-  //     action: handleSubmit(c),
-  //   },
-  // ];
+
   const updateIngredients = (e) => {
     e.preventDefault();
-    const data = { ActiveIngredients: activeIngredients }
-    console.log(data)
-    const response = axios.put(`http://localhost:8000/Pharmacist/UpdateMed/${username}/${name}`, data, {
-      headers: { authorization: "Bearer " + sessionStorage.getItem("token")},
-    })
-      .then(res => console.log(res.data)).catch(err => console.log(err))
-  }
-  const updatePrice = (e) => {
-    e.preventDefault();
-    const data = { Price: price }
-    console.log(data)
-    const response = axios.put(`http://localhost:8000/Pharmacist/UpdateMed/${username}/${name}`, data, {
-      headers: { authorization: "Bearer " + sessionStorage.getItem("token")},
-    })
-      .then(res => console.log(res.data)).catch(err => console.log(err))
-  }
-  const updatePicture = (e) => {
-    e.preventDefault();
-    const data = new FormData();
-    data.append('Picture', picture);
-    console.log(data);
+    const data = { ActiveIngredients: activeIngredients };
 
     axios.put(`http://localhost:8000/Pharmacist/UpdateMed/${username}/${name}`, data, {
       headers: { authorization: "Bearer " + sessionStorage.getItem("token")},
     })
-      .then(res => console.log(res.data))
+      .then(res => {
+        console.log(res.data);
+        alert('Active Ingredients updated successfully');
+        navigate(`/pharmacistView/${username}`);
+      })
+      .catch(err => console.log(err));
+  }
+
+  const updatePrice = (e) => {
+    e.preventDefault();
+    const data = { Price: price };
+
+    axios.put(`http://localhost:8000/Pharmacist/UpdateMed/${username}/${name}`, data, {
+      headers: { authorization: "Bearer " + sessionStorage.getItem("token")},
+    })
+      .then(res => {
+        console.log(res.data);
+        alert('Price updated successfully');
+        navigate(`/pharmacistView/${username}`);
+      })
+      .catch(err => console.log(err));
+  }
+
+  const updatePicture = (e) => {
+    e.preventDefault();
+    const data = { Picture: picture };
+
+    axios.put(`http://localhost:8000/Pharmacist/UpdateMed/${username}/${name}`, data, {
+      headers: { authorization: "Bearer " + sessionStorage.getItem("token")},
+    })
+      .then(res => {
+        console.log(res.data);
+        alert('Medicine Image updated successfully');
+        navigate(`/pharmacistView/${username}`);
+      })
       .catch(err => console.log(err));
   }
 
@@ -65,7 +62,7 @@ function EditMedicine() {
     <div>
       <NavBarPharmacist username={username}/>
       <h1>Edit Medicine</h1>
-      {/* <Form title="Edit Medicine" inputArr={inputArr} type="editMedicine" btnArr={btnArr} /> */}
+      
       <form>
         <h3>
           <input placeholder='active ingredients' type='text' onChange={(e) => setActiveIngredients(e.target.value)} />
@@ -80,14 +77,13 @@ function EditMedicine() {
         </h3>
 
         <h3>
-          <input placeholder='medicine image' type="file" accept="image/*" onChange={(e) => setPicture(e.target.files[0])} />
+          <input placeholder='medicine image URL' type="text" onChange={(e) => setPicture(e.target.value)} />
           <button onClick={updatePicture}>
             Update Medicine Image</button>
         </h3>
-
       </form>
-
     </div>
   );
 }
+
 export default EditMedicine;
