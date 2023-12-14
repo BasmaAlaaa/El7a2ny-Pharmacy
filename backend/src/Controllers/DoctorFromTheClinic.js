75e23542542a638c44063d6a6de7dba799196796
Mireille Bishay <110732750+mireillebishay@users.noreply.github.com>
2023-12-14 21:22:18 +0200

    get all meds

--- backend/src/Controllers/DoctorFromTheClinic.js
+++ backend/src/Controllers/DoctorFromTheClinic.js
@@ -20,6 +20,22 @@ const GetMedicineByDoctor = async (req, res) => {
   }
 }
 
+const GetAllMedicines = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  const { DoctorUsername } = req.params;
 
+  try {
+    const allMedicines = await Medicine.find({}, { _id: 0, ActiveIngredients: 0, Price: 0, Picture: 0, MedicalUse: 0 });
+    
+    if (allMedicines.length === 0) {
+      return res.status(404).json({ error: "No medicines found!" });
+    }
+    
+    res.status(200).json(allMedicines);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-module.exports = { GetMedicineByDoctor };
+module.exports = { GetMedicineByDoctor, GetAllMedicines };
