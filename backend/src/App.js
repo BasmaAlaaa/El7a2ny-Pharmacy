bee09b8deb76d96fc6ce05c4c362306b6e6a42e5
Mireille Bishay <110732750+mireillebishay@users.noreply.github.com>
2023-12-16 23:42:10 +0200

    cors * (change pass won't work)

--- backend/src/App.js
+++ backend/src/App.js
@@ -23,10 +23,9 @@ const io = new Server(server, {
 })
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: "*",
   credentials: true,
 }));
-
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
